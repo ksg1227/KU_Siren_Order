11b492d18e1143ea256f7398547e78567769dfaf
beom84 <cjja00@konkuk.ac.kr>
2024-06-11 17:46:08 +0900

    dialog 디자인 순서

--- app/src/main/java/com/example/teamproject/Component/CustomDialog.kt
+++ app/src/main/java/com/example/teamproject/Component/CustomDialog.kt
@@ -1,7 +1,8 @@
 package com.example.teamproject.Component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -20,7 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -28,6 +29,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.example.teamproject.R
 
 @Composable
 fun CustomAlertDialog(
@@ -49,7 +51,7 @@ fun CustomAlertDialog(
             Column(
                 modifier = Modifier
                     .width(300.dp)
-                    .height(200.dp)
+                    .height(230.dp)
                     .background(
                         color = Color.White,
                     ),
@@ -66,15 +68,9 @@ fun CustomAlertDialog(
                         fontSize = 16.sp,
                         fontWeight = FontWeight.Bold
                     )
-                    , modifier = Modifier.padding(bottom = 15.dp)
                 )
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(color = Color.LightGray)
-                )
+
                 Row(
                     modifier = Modifier
                         .weight(1f)
@@ -83,21 +79,24 @@ fun CustomAlertDialog(
                 ) {
                     Button(
                         onClick = { onClickToFloor() },
-                        shape = RectangleShape,
+                        shape = RoundedCornerShape(8.dp),
                         modifier = Modifier
                             .weight(1f)
-                            .fillMaxWidth()
-                            .padding(15.dp),
+                            .fillMaxHeight()
+                            .padding(bottom = 20.dp, end = 10.dp, top = 15.dp, start = 20.dp)
+                            .border(
+                                width = 1.dp, color = Color.Gray
+                            ),
                         colors = ButtonDefaults.buttonColors(
                             containerColor = Color.White, // 버튼 배경색상
-                            contentColor = Color.Black, // 버튼 텍스트 색상
+                            contentColor =  Color(0xFF444444), // 버튼 텍스트 색상
                             disabledContainerColor = Color.Gray, // 버튼 비활성화 배경 색상
                             disabledContentColor = Color.White, // 버튼 비활성화 텍스트 색상
                         ),
 
                         ) {
                         Text(
-                            text = "학생회관 1층 학식",
+                            text = "학생회관       1층 학식",
                             textAlign = TextAlign.Center,
                             style = TextStyle(
                                 fontSize = 14.sp,
@@ -106,29 +105,27 @@ fun CustomAlertDialog(
                         )
                     }
 
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .width(1.dp)
-                            .background(color = Color.LightGray)
-                    )
+
 
                     Button(
                         onClick = { onClickToBottom() },
-                        shape = RectangleShape,
+                        shape = RoundedCornerShape(8.dp),
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxHeight()
-                            .padding(15.dp),
+                            .padding(bottom = 20.dp, end = 20.dp, top = 15.dp, start = 10.dp)
+                            .border(
+                                width = 1.dp, color = Color.Gray
+                            ),
                         colors = ButtonDefaults.buttonColors(
                             containerColor = Color.White, // 버튼 배경색상
-                            contentColor = Color.Red, // 버튼 텍스트 색상
+                            contentColor = Color(0xFF444444), // 버튼 텍스트 색상
                             disabledContainerColor = Color.Gray, // 버튼 비활성화 배경 색상
                             disabledContentColor = Color.White, // 버튼 비활성화 텍스트 색상
                         ),
                     ) {
                         Text(
-                            text = "학생회관 지하 학식",
+                            text = "학생회관    지하 학식",
                             textAlign = TextAlign.Center,
                             style = TextStyle(
                                 fontSize = 14.sp,
@@ -138,25 +135,22 @@ fun CustomAlertDialog(
                     }
                 }
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(color = Color.LightGray)
-                )
+
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(IntrinsicSize.Min) // Row의 높이를 내부 컴포넌트에 맞춤
+                        .height(IntrinsicSize.Min)// Row의 높이를 내부 컴포넌트에 맞춤
+                        .padding(bottom = 20.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
                     Button(
                         onClick = { onClickCancel() },
-                        shape = RectangleShape,
+                        shape = RoundedCornerShape(8.dp),
                         modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
+                            .height(40.dp)
+                            .width(130.dp),
                         colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.White, // 버튼 배경색상
+                            containerColor = colorResource(id = R.color.green_65a25b), // 버튼 배경색상
                             contentColor = Color.Black, // 버튼 텍스트 색상
                             disabledContainerColor = Color.Gray, // 버튼 비활성화 배경 색상
                             disabledContentColor = Color.White, // 버튼 비활성화 텍스트 색상

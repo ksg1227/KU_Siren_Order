ecf3c81f7a1b9baff684fd1e2444ec79f49dbc23
beom84 <cjja00@konkuk.ac.kr>
2024-06-14 15:46:22 +0900

    navigation 일부 수정

--- app/src/main/java/com/example/teamproject/Screen/LoginScreen.kt
+++ app/src/main/java/com/example/teamproject/Screen/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.example.teamproject.Screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
